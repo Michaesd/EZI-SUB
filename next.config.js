29bc691640e9c6708810f02bb9470778f04b934a
Gita Kusuma <8600551@gmail.com>
2026-04-22 21:21:56 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // 我们只保留这一行，其他什么都不要！
-  // 它清晰地告诉Next.js，这是一个输出给独立服务器（比如Cloudflare）的优化版本。
-  output: "standalone",
+  // 在 Vercel 部署，我们不需要 output: 'standalone'，删掉它反而更稳定
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
